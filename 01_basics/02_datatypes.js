c33636dff896584e9e06f1c9e8260aad934ffe5e
JEEVAN <47097581+JeevanKatiyar@users.noreply.github.com>
2024-03-18 11:45:33 +0000

    conversion

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict";//treat all JS code as neer version.
+
+//alert(3+3); we are using nodejs not browser
+//code readability should be high.
+
+console.log("dataTypes");
+
+let Sname ="jeevan";
+let age =18;
+
+let isLoggedIn =false;
+
+/* 
+PRIMITIVE DATA TYPES
+
+number= 2 to power 53
+bigit
+string= ""
+boolean = true/false
+null = standalone value - empty value. 
+
+NOTE: 
+NULL typeof is object ***
+undefined typeof is undefined
+
+
+undefined =  value not assigned yet
+symbol = uniqueness
+
+OBJECT  
+*/
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = 32
+console.log(typeof(score));
+/*
+
+"33" = 33
+"33abc" = NaN
+true = 1
+false =0
+
+*/
+
+let isLoggedIn ="jeevan"
+let Bool = Boolean(isLoggedIn)
+console.log(Bool);
+
+// 1= true;
+// 0= falsel;
+// "" = false;
+// "jeevan" = true;
+
+//converting operation
+let someNumber =90;
+let convertString = String(someNumber);
+console.log(convertString);
+console.log(typeof convertString);
