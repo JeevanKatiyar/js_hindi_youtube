92c8d4c4c8c8d2ec9fddda2339381962930f3a15
JEEVAN <47097581+JeevanKatiyar@users.noreply.github.com>
2024-03-18 16:25:51 +0000

    datatype-summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,31 @@
+/*
+Primitive and non primitive/reference type
+
+Primitive data type: call by value
+
+1- String
+2- Number
+3- Boolean
+4- Null
+5- Undefined
+6- BigInt
+7- Symbol
+
+
+refrence type /Non Primitive
+1- Array
+2- Objects
+*/
+const heros = ["shaktiman","maharaja","doga"];
+let myDetails = {
+    name:"jeevan",
+    number :"90800",
+    place : "hyd",
+}
+
+const myFunction =function(){
+    console.log("hello");
+}
+console.table(myDetails);
+//Note: Javascript is a Dynamically Typed Language
+ //*/
