ce21fe50eb71edeee83cbbb32550fdd9e4232b9b
JEEVAN <47097581+JeevanKatiyar@users.noreply.github.com>
2024-03-18 12:06:08 +0000

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,25 +1,52 @@
-let score = 32
-console.log(typeof(score));
-/*
+// let score = 32
+// console.log(typeof(score));
+// /*
 
-"33" = 33
-"33abc" = NaN
-true = 1
-false =0
+// "33" = 33
+// "33abc" = NaN
+// true = 1
+// false =0
 
-*/
+// */
 
-let isLoggedIn ="jeevan"
-let Bool = Boolean(isLoggedIn)
-console.log(Bool);
+// let isLoggedIn ="jeevan"
+// let Bool = Boolean(isLoggedIn)
+// console.log(Bool);
 
 // 1= true;
 // 0= falsel;
 // "" = false;
 // "jeevan" = true;
 
-//converting operation
-let someNumber =90;
-let convertString = String(someNumber);
-console.log(convertString);
-console.log(typeof convertString);
+//converting 
+// let someNumber =90;
+// let convertString = String(someNumber);
+// console.log(convertString);
+// console.log(typeof convertString);
+
+//<><><><><><><><><>opeation<><><><><><><><><>
+// let value =9;
+// let negValue =-value
+// console.log(negValue);
+
+// let str1 ="hey ";
+// let str2 = "jeevan"
+// let str3 = str1+str2;
+// console.log(str3);
+
+//complex situation
+// note: 
+// if string is first everything is converted into string,
+// if number is first then mathmatical operations are performed
+
+//tricky conversion;
+
+console.log(+true); 
+
+// notes to study:
+//https://tc39.es/ecma262/#sec-abstract-operations
+
+
+
+
+
