477dff512780e4fc32dbb0d0592c3e95b219ea63
JEEVAN <47097581+JeevanKatiyar@users.noreply.github.com>
2024-03-18 08:09:58 +0000

    learnt about varables and datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+let sName ="jeevan";
+var place ='hyderabad';
+const number=98877566;
+
+console.log(number);
+console.table([sName, place, number]);
+
+/*
+prefer not to use var :
+                because of issue in block scope and fuctional scope
+*/
+
