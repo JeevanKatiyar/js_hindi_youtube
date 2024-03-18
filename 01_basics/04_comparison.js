c7a61743214d051d1a732572624af4c2c16eaf79
JEEVAN <47097581+JeevanKatiyar@users.noreply.github.com>
2024-03-18 12:18:47 +0000

    comaprison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,13 @@
+//comparison
+ 
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null === 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+// avoid this type of comparison
